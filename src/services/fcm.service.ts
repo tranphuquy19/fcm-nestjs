003822450459a0c998b9b7581043d7aa666626cc
tranphuquy19 <christian@enclave.vn>
2021-07-16 05:14:40 +0700

    first commit

---
+++ src/services/fcm.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import { Inject, Logger } from '@nestjs/common';
+import { FCM_OPTIONS } from '../fcm.constants';
+import { FcmOptions } from '../interfaces/fcm-options.interface';
+import * as firebaseAdmin from 'firebase-admin';
+
+@Injectable()
+export class FcmService {
+  constructor(
+    @Inject(FCM_OPTIONS) private fcmOptionsProvider: FcmOptions,
+    private readonly logger: Logger,
+  ) { }
+
+  async sendNotification(
+    deviceIds: Array<string>,
+    payload: firebaseAdmin.messaging.MessagingPayload,
+    silent: boolean,
+  ) {
+    if (deviceIds.length == 0) {
+      throw new Error('You provide an empty device ids list!');
+    }
+
+    if (firebaseAdmin.apps.length === 0) {
+      firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(
+          this.fcmOptionsProvider.firebaseSpecsPath,
+        ),
+      });
+    }
+
+    const options = {
+      priority: 'high',
+      timeToLive: 60 * 60 * 24,
+    };
+
+    const optionsSilent = {
+      priority: 'high',
+      timeToLive: 60 * 60 * 24,
+      content_available: true,
+    };
+
+    let result = null;
+    try {
+      result = await firebaseAdmin
+        .messaging()
+        .sendToDevice(deviceIds, payload, silent ? optionsSilent : options);
+    } catch (error) {
+      this.logger.error(error.message, error.stackTrace, 'nestjs-fcm');
+      throw error;
+    }
+    return result;
+  }
+
+  /**
+   * 
+   * @param topic `all` is send to all devices
+   * @param payload ref: firebaseAdmin.messaging.MessagingPayload
+   * @param silent 
+   * @returns 
+   */
+  async sendToTopic(
+    topic: 'all' | string,
+    payload: firebaseAdmin.messaging.MessagingPayload,
+    silent: boolean
+  ) {
+    if (topic.trim().length === 0) {
+      throw new Error('You provide an empty topic name!');
+    }
+
+    if (firebaseAdmin.apps.length === 0) {
+      firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(
+          this.fcmOptionsProvider.firebaseSpecsPath,
+        ),
+      });
+    }
+
+    const options = {
+      priority: 'high',
+      timeToLive: 60 * 60 * 24,
+    };
+
+    const optionsSilent = {
+      priority: 'high',
+      timeToLive: 60 * 60 * 24,
+      content_available: true,
+    };
+
+    let result = null;
+    try {
+      result = await firebaseAdmin
+        .messaging()
+        .sendToTopic(topic, payload, silent ? optionsSilent : options);
+    } catch (error) {
+      this.logger.error(error.message, error.stackTrace, 'nestjs-fcm');
+      throw error;
+    }
+    return result;
+  }
+}
